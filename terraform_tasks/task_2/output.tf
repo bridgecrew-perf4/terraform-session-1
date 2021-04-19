66d1ac405fdec68effc684ca995f78668b8f0bf7
nazy67 <nazykh67@gmail.com>
2021-04-19 06:05:30 +0000

    modified output.tf

--- terraform_tasks/task_2/output.tf
+++ terraform_tasks/task_2/output.tf
@@ -7,11 +7,14 @@ output "wordpress_private_ip" {
 output "wordpressdb_public_ip" {
   value = aws_instance.wordpress_db.public_ip
 }
+output "wordpressdb_private_ip" {
+  value = aws_instance.wordpress_db.private_ip
+}
 output "wp_sg_name" {
   value = aws_security_group.wordpress_sg.name
 }
-output "public_dns" {
-  value = aws_instance.wordpress_web.public_dns
+output "wpdb_sg_name" {
+  value = aws_security_group.wordpress_db_sg.name
 }
 output "dns_name" {
   value = aws_route53_record.record.name
