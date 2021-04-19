92355786e71f570b2e8f71ef92ab199c24e7246b
nazy67 <nazykh67@gmail.com>
2021-04-19 04:44:23 +0000

    modified cidr_blocks variable

--- session_5/qa/tfvars/dev.tf
+++ session_5/qa/tfvars/dev.tf
@@ -1,11 +1,12 @@
 env           = "dev"
 instance_type = "t2.micro"
-#image_id      = "ami-0742b4e673072066f"
 key_name      = "my_tf_key"
 http_inbound_rule = "80"
 ssh_inbound_rule = "22"
 outbound_rule = "0"
-cidr_blocks = ["0.0.0.0/0"]
+#cidr_blocks = ["0.0.0.0/0"]
 sg_name = "web_sg"
 ingress_protocol = "tcp"
 egress_protocol = "-1"
+web_sg_tcp_ports = ["22", "3306", "80"]
+web_sg_tcp_ports_cidr = ["0.0.0.0/0", "10.0.1.0/32", "10.0.2.0/32"]
