4acc2cda2769b5a81889a4cd86e906d2145deb90
nazy67 <nazykh67@gmail.com>
2021-04-19 02:15:20 +0000

    added content to sql_userdata.sh

--- terraform_tasks/task_2/sql_userdata.sh
+++ terraform_tasks/task_2/sql_userdata.sh
@@ -4,13 +4,36 @@ sudo hostnamectl set-hostname wordpress-db
 sudo yum install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-#sudo mysql_secure_installation <<EOF
+sudo mysql_secure_installation <<EOF
+
+y
+password123
+password123
+y
+y
+y
+y
+EOF
+mysql -uroot -ppassword123<<MYSQL_SCRIPT
+CREATE DATABASE wordpressdb;
+CREATE USER wordpressuser@'${wordpress_private_ip}' IDENTIFIED BY 'redhat123';
+GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@'${wordpress_private_ip}' IDENTIFIED BY 'redhat123';
+FLUSH PRIVILEGES;
+EXIT;
+MYSQL_SCRIPT
+sudo systemctl restart mariadb
+
+
+##!/bin/bash
+#PASS=`pwgen -s 40 1`
 #
-#y
-#redhat123
-#redhat123
-#y
-#y
-#y
-#y
-#EOF
+#mysql -uroot <<MYSQL_SCRIPT
+#CREATE DATABASE $1;
+#CREATE USER '$1'@'localhost' IDENTIFIED BY '$PASS';
+#GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';
+#FLUSH PRIVILEGES;
+#MYSQL_SCRIPT
+#
+#echo "MySQL user created."
+#echo "Username:   $1"
+#echo "Password:   $PASS"
