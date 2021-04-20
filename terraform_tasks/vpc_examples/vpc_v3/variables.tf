8e1cf820af1858a42789baf4042333ed69895eb8
nazy67 <nazykh67@gmail.com>
2021-04-20 00:55:09 +0000

    modified vpc.tf, variables.tf, tfvars.dev.tf

--- terraform_tasks/vpc_examples/vpc_v3/variables.tf
+++ terraform_tasks/vpc_examples/vpc_v3/variables.tf
@@ -30,27 +30,6 @@ variable "rt_cidr_block" {
   description = "route table cidr block"
 }
 
-# Subnet variables
-variable "pub_cidr_subnet" {
-  type        = map(any)
-  description = "The availabitily zones where terraform deploys your infra"
-  default = {
-    "us-east-1a" = "10.0.1.0/24"
-    "us-east-1b" = "10.0.2.0/24"
-    "us-east-1c" = "10.0.3.0/24"
-  }
-}
-
-#variable "priv_cidr_subnet" {
-#  type        = list(any)
-#  description = "cidr blocks for the private subnets"
-#    default = {
-#    "us-east-1a" = "10.0.11.0/24"
-#    "us-east-1b" = "10.0.12.0/24"
-#    "us-east-1c" = "10.0.13.0/24"
-#  }
-#}
-
 # Tags variables
 variable "env" {
   type        = string
--- terraform_tasks/vpc_examples/vpc_v3/vpc.tf
+++ terraform_tasks/vpc_examples/vpc_v3/vpc.tf
@@ -1,3 +1,8 @@
+# In this template for_each function with local variables were used in the creation of
+# resources. for_each can work with a string or a map values. In our case we are
+# working with map, here we can pass separate settings for each subnet, while using 
+# the keys for generating subnets.
+
 # VPC 
 resource "aws_vpc" "my_vpc" {
   cidr_block           = var.vpc_cidr_block
@@ -14,129 +19,125 @@ resource "aws_vpc" "my_vpc" {
 
 # Public Subnets
 resource "aws_subnet" "public_subnet_" {
-  for_each = var.pub_cidr_subnet
-
+  for_each = local.public_subnet
   vpc_id            = aws_vpc.my_vpc.id
-  availability_zone = each.key
-  cidr_block        = each.value
+  availability_zone = each.value.availability_zone
+  cidr_block        = each.value.cidr_block
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_pub_sub_${count.index}"
+      Name = "${var.env}_pub_sub_${each.key}"
+    }
+  )
+}
+locals {
+  public_subnet = {
+    1 = { availability_zone = "us-east-1a", cidr_block = "10.0.1.0/24" },
+    2 = { availability_zone = "us-east-1b", cidr_block = "10.0.2.0/24" },
+    3 = { availability_zone = "us-east-1c", cidr_block = "10.0.3.0/24" }
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_subnet_" {
+  for_each = local.private_subnet
+  vpc_id            = aws_vpc.my_vpc.id
+  availability_zone = each.value.availability_zone
+  cidr_block        = each.value.cidr_block
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_priv_sub_${each.key}"
     }
   )
 }
 
-## Private Subnets
-#resource "aws_subnet" "private_subnet_" {
-#  count             = length(var.subnet_azs)
-#  vpc_id            = aws_vpc.my_vpc.id
-#  cidr_block        = element(var.priv_cidr_subnet, count.index)
-#  availability_zone = element(var.subnet_azs, count.index)
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_priv_sub_${count.index}"
-#    }
-#  )
-#}
-#
-## Internet Gateway
-#resource "aws_internet_gateway" "igw" {
-#  vpc_id = aws_vpc.my_vpc.id
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_igw"
-#    }
-#  )
-#}
-#
-## Public Route Table
-#resource "aws_route_table" "pub_rtb" {
-#  vpc_id = aws_vpc.my_vpc.id
-#
-#  route {
-#    cidr_block = var.rt_cidr_block
-#    gateway_id = aws_internet_gateway.igw.id
-#  }
-#
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_pub_rtb"
-#    }
-#  )
-#}
-#
-## Public Route Table Association
-#resource "aws_route_table_association" "pub_sub1" {
-#  subnet_id      = aws_subnet.public_subnet_[0].id
-#  route_table_id = aws_route_table.pub_rtb.id
-#}
-#
-#resource "aws_route_table_association" "pub_sub2" {
-#  subnet_id      = aws_subnet.public_subnet_[1].id
-#  route_table_id = aws_route_table.pub_rtb.id
-#}
-#
-#resource "aws_route_table_association" "pub_sub3" {
-#  subnet_id      = aws_subnet.public_subnet_[2].id
-#  route_table_id = aws_route_table.pub_rtb.id
-#}
-#
-## Elastic IP
-#resource "aws_eip" "nat_gw_eip" {
-#  vpc = true
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_eip"
-#    }
-#  )
-#}
-#
-## Nat Gateway
-#resource "aws_nat_gateway" "nat_gw" {
-#  depends_on    = [aws_internet_gateway.igw]
-#  allocation_id = aws_eip.nat_gw_eip.id
-#  subnet_id     = aws_subnet.public_subnet_[0].id
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_nat_gw"
-#    }
-#  )
-#}
-#
-## Private Route Table
-#resource "aws_route_table" "private_rtb" {
-#  vpc_id = aws_vpc.my_vpc.id
-#
-#  route {
-#    cidr_block     = var.rt_cidr_block
-#    nat_gateway_id = aws_nat_gateway.nat_gw.id
-#  }
-#  tags = merge(
-#    local.common_tags,
-#    {
-#      Name = "${var.env}_private_rtb"
-#    }
-#  )
-#}
-#
-## Private Route Table Association
-#resource "aws_route_table_association" "priv_sub1" {
-#  subnet_id      = aws_subnet.private_subnet_[0].id
-#  route_table_id = aws_route_table.private_rtb.id
-#}
-#
-#resource "aws_route_table_association" "priv_sub2" {
-#  subnet_id      = aws_subnet.private_subnet_[1].id
-#  route_table_id = aws_route_table.private_rtb.id
-#}
-#
-#resource "aws_route_table_association" "priv_sub3" {
-#  subnet_id      = aws_subnet.private_subnet_[2].id
-#  route_table_id = aws_route_table.private_rtb.id
-#}
+locals {
+  private_subnet = {
+    1 = { availability_zone = "us-east-1a", cidr_block = "10.0.11.0/24" },
+    2 = { availability_zone = "us-east-1b", cidr_block = "10.0.12.0/24" },
+    3 = { availability_zone = "us-east-1c", cidr_block = "10.0.13.0/24" }
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_igw"
+    }
+  )
+}
+
+# Public Route Table
+resource "aws_route_table" "pub_rtb" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = var.rt_cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_pub_rtb"
+    }
+  )
+}
+
+# Public Route Table Association
+resource "aws_route_table_association" "pub_subnet" {
+  for_each = local.public_subnet
+  subnet_id      = aws_subnet.public_subnet_[each.key].id
+  route_table_id = aws_route_table.pub_rtb.id
+}
+
+# Elastic IP
+resource "aws_eip" "nat_gw_eip" {
+  vpc = true
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_eip"
+    }
+  )
+}
+
+# Nat Gateway
+resource "aws_nat_gateway" "nat_gw" {
+  depends_on    = [aws_internet_gateway.igw]
+  allocation_id = aws_eip.nat_gw_eip.id
+  subnet_id     = aws_subnet.public_subnet_[1].id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_nat_gw"
+    }
+  )
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rtb" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block     = var.rt_cidr_block
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_private_rtb"
+    }
+  )
+}
+
+# Private Route Table Association
+resource "aws_route_table_association" "priv_subnet" {
+  for_each = local.private_subnet
+  subnet_id      = aws_subnet.private_subnet_[each.key].id
+  route_table_id = aws_route_table.private_rtb.id
+}
