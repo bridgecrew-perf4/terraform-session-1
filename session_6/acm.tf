cc34174292f4d8635d9401c0a6da50764d5060d7
nazy67 <nazykh67@gmail.com>
2021-04-26 17:56:08 +0000

    modified acm.tf

--- session_6/acm.tf
+++ session_6/acm.tf
@@ -1,15 +1,8 @@
-# Find a certificate that is issued
+# Fetch a certificate that is issued
 data "aws_acm_certificate" "my_certificate" {
   domain   = "nazydaisy.com"
   statuses = ["ISSUED"]
 }
-
-# # Find a certificate issued by (not imported into) ACM
-# data "aws_acm_certificate" "my_certificate" {
-#   domain      = "www.nazydaizy.com"
-#   types       = ["AMAZON_ISSUED"]
-#   most_recent = false #true
-# }
 
 resource "aws_lb_listener_certificate" "acm_attachment" {
   listener_arn    = aws_lb_listener.https_listener.arn
@@ -24,6 +17,11 @@ resource "aws_route53_record" "record" {
   zone_id = data.aws_route53_zone.my_zone.zone_id
   name    = "www.${data.aws_route53_zone.my_zone.name}"
   type    = "A"
-  ttl     = "300"
-  records = [aws_instance.wordpress_web.public_ip]
+
+  alias {
+    name                   = aws_lb.web_lb.dns_name
+    zone_id                = aws_lb.web_lb.zone_id
+    evaluate_target_health = true
+  }
+
 }
