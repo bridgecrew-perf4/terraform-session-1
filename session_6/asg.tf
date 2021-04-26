e11e1aa7a98dab3a7ce2686df3028790a823f0ed
nazy67 <nazykh67@gmail.com>
2021-04-26 07:01:05 +0000

    modified locals.tf, asg.tf

--- session_6/asg.tf
+++ session_6/asg.tf
@@ -1,18 +1,16 @@
 resource "aws_autoscaling_group" "web_asg" {
-  for_each          = local.public_subnet
   name                      = "${var.env}_web_asg"
-  max_size                  = 2
+  max_size                  = 5
   min_size                  = 1
   health_check_grace_period = 300
   health_check_type         = "ELB" # "EC2"
-  desired_capacity          = 1
+  desired_capacity          = 3
   force_delete              = true
   launch_configuration      = aws_launch_configuration.web_lc.name
-  vpc_zone_identifier       = [aws_subnet.public_subnet[each.key].id]
+  vpc_zone_identifier       = [aws_subnet.public_subnet[1].id, aws_subnet.public_subnet[2].id, aws_subnet.public_subnet[3].id]
 }
 
 resource "aws_autoscaling_attachment" "web_lb_asg_attachment" {
-  for_each          = local.public_subnet
   alb_target_group_arn   = aws_lb_target_group.web_tg.arn
-  autoscaling_group_name = aws_autoscaling_group.web_asg[each.key].id
+  autoscaling_group_name = aws_autoscaling_group.web_asg.id
 }
--- session_6/locals.tf
+++ session_6/locals.tf
@@ -20,3 +20,59 @@ locals {
     3 = { availability_zone = "us-east-1c", cidr_block = "10.0.13.0/24" }
   }
 }
+
+locals {
+  ingress_rules = {
+    1 = { from_port = 443, to_port = 443, protocol = "tcp", cidr_block = "0.0.0.0/0", description = "https_ingress", type = "ingress" },
+    2 = { from_port = 80, to_port = 80, protocol = "tcp", cidr_block = "0.0.0.0/0", description = "http_ingress", type = "ingress" },
+  }
+}
+
+locals {
+  cloudwatch_alarm = {
+    scale_out = { comparison_operator = "GreaterThanOrEqualToThreshold",
+      alarm_name         = "${var.env}_scale_out_alarm"
+      evaluation_periods = "2",
+      protocol           = "tcp",
+      metric_name        = "CPUUtilization",
+      namespace          = "AWS/EC2",
+      period             = "120",
+      statistic          = "Average"
+      threshold          = "60"
+      alarm_description  = "This metric monitors webserver cpu utilization for scaling out"
+      alarm_actions      = [aws_autoscaling_policy.asg_scale_policy[1].arn]
+    },
+    scale_in = { comparison_operator = "LessThanOrEqualToThreshold",
+      alarm_name         = "${var.env}_scale_in_alarm"
+      evaluation_periods = "2",
+      protocol           = "tcp",
+      metric_name        = "CPUUtilization",
+      namespace          = "AWS/EC2",
+      period             = "120",
+      statistic          = "Average"
+      threshold          = "40"
+      alarm_description  = "This metric monitors webserver cpu utilization for scaling in"
+      alarm_actions      = [aws_autoscaling_policy.asg_scale_policy[2].arn]
+    },
+  }
+}
+
+locals {
+  asg_scale_policy = {
+    1 = { name = "${var.env}_asg_out_policy",
+      scaling_adjustment     = 1,
+      adjustment_type        = "ChangeInCapacity",
+      cooldown               = 300,
+      autoscaling_group_name = aws_autoscaling_group.web_asg.name
+      policy_type            = "SimpleScaling"
+    },
+    2 = { name = "${var.env}_asg_in_policy",
+      scaling_adjustment     = 1,
+      adjustment_type        = "ChangeInCapacity",
+      cooldown               = 300,
+      description            = "http_ingress",
+      autoscaling_group_name = aws_autoscaling_group.web_asg.name
+      policy_type            = "SimpleScaling"
+    },
+  }
+}
