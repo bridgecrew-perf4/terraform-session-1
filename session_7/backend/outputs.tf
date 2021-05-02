1a87543901963cf54affe3930bab930eb6ca75b3
nazy67 <nazykh67@gmail.com>
2021-05-02 23:39:48 +0000

    modified outputs.tf

--- session_7/backend/outputs.tf
+++ session_7/backend/outputs.tf
@@ -1,3 +1,15 @@
 output "rds_password" {
   value = random_password.password.result
 }
+
+output "rds_db_name" {
+  value = aws_db_instance.rds_db.name
+}
+
+output "rds_db_username" {
+  value = aws_db_instance.rds_db.username
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds_db.address
+}
