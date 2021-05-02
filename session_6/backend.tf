32324f961cef6c37c220df8d54042689f863b9cc
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-05-01 23:39:17 -0500

    Delete session_6 directory

--- session_6/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "nazy-tf-bucket"
-    key    = "session_6/frontend.tfstate"
-    region = "us-east-1"
-  }
-}
--- session_6/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "lb_dns_name" {
-  value = aws_lb.web_lb.dns_name
-}
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
