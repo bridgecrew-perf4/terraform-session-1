3cc062d82c0085ad1581be47594a0857b14dc07a
nazy67 <nazykh67@gmail.com>
2021-05-01 23:28:00 -0500

    modified asg.tf

--- terraform_tasks/task_3/asg.tf
+++ terraform_tasks/task_3/asg.tf
@@ -18,7 +18,27 @@ resource "aws_autoscaling_group" "web_asg" {
   ]
 }
 
+#   tags = merge(
+    
+#       {
+#         key                 = "Name"
+#         value               = "${var.env}_web_asg"
+#         propagate_at_launch = true
+#       },
+#       {
+#         key                 = "Environment"
+#         value               = var.env
+#         propagate_at_launch = true
+#       },
+#     local.common_tags,
+#   )
+# }
+
 resource "aws_autoscaling_attachment" "web_lb_asg_attachment" {
   alb_target_group_arn   = aws_lb_target_group.web_tg.arn
   autoscaling_group_name = aws_autoscaling_group.web_asg.id
+}
+
+output "tag" {
+  value = local.common_tags
 }
