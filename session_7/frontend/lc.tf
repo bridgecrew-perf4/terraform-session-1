264644fab87e46e9e320c7322ff208e773b3dd74
nazy67 <nazykh67@gmail.com>
2021-05-03 01:18:56 +0000

    modified sg of webserver

--- session_7/frontend/lc.tf
+++ session_7/frontend/lc.tf
@@ -10,6 +10,12 @@ resource "aws_security_group" "web_sg" {
   name        = "${var.env}_web_sg"
   description = "Allow HTTP traffic"
   #vpc must be here if it is not default VPC. In our case, it is defulat VPC)
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_web_sg"
+    }
+  )  
 }
 
 resource "aws_security_group_rule" "http_from_lb" {
@@ -18,6 +24,15 @@ resource "aws_security_group_rule" "http_from_lb" {
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "mysql_to_db" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = data.terraform_remote_state.rds.outputs.rds_sg_id
   security_group_id = aws_security_group.web_sg.id
 }
 resource "aws_security_group_rule" "web_egress" {
