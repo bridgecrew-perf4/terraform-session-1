5d00ab881b86d03e880ef7db2a6498b7b3d3d482
nazy67 <nazykh67@gmail.com>
2021-05-03 05:11:05 +0000

    added tempalet_file folder

---
+++ session_7/frontend/template_file/user_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo amazon-linux-extras install php8.0 -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+sudo yum install php-gd -y
+sudo yum install mysql -y
+sudo cp -r wordpress/* /var/www/html
+sudo chown -R apache:apache /var/www/html
+sudo systemctl restart httpd
