2e5c772d14c5d5624b35328e994fcd87399500f9
nazy67 <nazykh67@gmail.com>
2021-05-04 07:44:30 +0000

    added launch_template.tf

--- terraform_tasks/task_3.1/launch_template.tf
+++ terraform_tasks/task_3.1/launch_template.tf
@@ -1,10 +1,21 @@
-# Launch configuration
-resource "aws_launch_configuration" "web_lc" {
-  name            = "${var.env}_web_lc"
-  image_id        = data.aws_ami.amazon_linux2.id
-  instance_type   = var.instance_type
-  user_data       = data.template_file.webserver.rendered
-  security_groups = [aws_security_group.web_sg.id]
+# Launch Template
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "${var.env}_web_template"
+  image_id      = data.aws_ami.amazon_linux2.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+  user_data       = filebase64("${path.module}/user_data.sh")
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_web_lt"
+    }
+  )
 }
 
 # Security group for webserver
@@ -12,6 +23,13 @@ resource "aws_security_group" "web_sg" {
   name        = "${var.env}_web_sg"
   description = "allow http traffic"
   vpc_id      = aws_vpc.my_vpc.id
+  
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_web_sg"
+    }
+  )
 }
 
 resource "aws_security_group_rule" "http_from_lb" {
@@ -27,7 +45,7 @@ resource "aws_security_group_rule" "web_egress" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  protocol          = "-1" # -1 means everything
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.web_sg.id
 }
