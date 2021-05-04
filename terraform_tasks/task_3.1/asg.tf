0b883e3a254fd7d6bf190471b42f815f4f5e1ca6
nazy67 <nazykh67@gmail.com>
2021-05-04 07:43:09 +0000

    modified asg.tf

--- terraform_tasks/task_3.1/asg.tf
+++ terraform_tasks/task_3.1/asg.tf
@@ -1,16 +1,20 @@
 resource "aws_autoscaling_group" "web_asg" {
   name = "${var.env}_web_asg"
-
   max_size         = 3
   min_size         = 1
   desired_capacity = 3
-
-  health_check_grace_period = 120   #300 by default
-  health_check_type         = "ELB" # or "EC2" 
+  health_check_type         = "ELB" 
   force_delete              = true
   target_group_arns         = [aws_lb_target_group.web_tg.arn]
-  launch_configuration      = aws_launch_configuration.web_lc.name
-  vpc_zone_identifier       = ["aws_subnet.private_subnet[*].id"]
+  # vpc_zone_identifier =  ["${aws_subnet.private_subnet[*].id}"]
+  vpc_zone_identifier       = [aws_subnet.private_subnet[0].id,
+                               aws_subnet.private_subnet[1].id,
+                               aws_subnet.private_subnet[2].id]                             
+                       
+  launch_template {
+    id      = aws_launch_template.web_template.id
+    version = aws_launch_template.web_template.latest_version
+  }
 
   tag {
     key                 = "Name"
@@ -28,6 +32,7 @@ resource "aws_autoscaling_group" "web_asg" {
     }
   }
 }
+
 resource "aws_autoscaling_attachment" "web_lb_asg_attachment" {
   alb_target_group_arn   = aws_lb_target_group.web_tg.arn
   autoscaling_group_name = aws_autoscaling_group.web_asg.id
