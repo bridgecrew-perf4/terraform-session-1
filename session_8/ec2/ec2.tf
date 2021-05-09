d926eba8d26c0b673c6da84edabcbcb812bd5e17
nazy67 <nazykh67@gmail.com>
2021-05-09 23:22:42 +0000

    modified ec2.tf

--- session_8/ec2/ec2.tf
+++ session_8/ec2/ec2.tf
@@ -2,11 +2,21 @@
 resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.amazon_linux2.image_id # data source reference
   instance_type          = var.instance_type                   # variable
-  key_name               = aws_key_pair.tf_key.key_name        # reference to key pair
-  vpc_security_group_ids = [aws_security_group.web_sg.id]      # reference to security group 
-
+  key_name               = aws_key_pair.tf_key.key_name        # resource reference to key pair
+  vpc_security_group_ids = [aws_security_group.web_sg.id]      # resource reference to security group 
   tags = local.common_tags
-}
+
+  provisioner "file" {     # when we wrap it inside of the ec2 block it will be like user data
+    source = "index.html"
+    destination = "/tmp/index.html"  # we are putting under tmp because we don't have var/www/hmtl/index.html we didn't install apache yet
+  }  
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+      private_key = file("~/.ssh/id_rsa") # Terraform will go to remote machine and establish connection with private key matching with public key, which was already created with public key of the local machine
+    }
+  }
 
 # Key Pair
 resource "aws_key_pair" "tf_key" {
