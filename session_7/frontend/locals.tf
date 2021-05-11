5858e0ef24b19afdfda0da92a06650854c0eb769
nazy67 <nazykh67@gmail.com>
2021-05-11 02:34:46 +0000

    modified locals.tf

--- session_7/frontend/locals.tf
+++ session_7/frontend/locals.tf
@@ -6,14 +6,3 @@ locals {
     Owner       = "Nazy"
   }
 }
-
-locals {
-  ingress_rules = {
-    http = { from_port = 80,
-      to_port     = 80,
-      protocol    = "tcp",
-      cidr_block  = "0.0.0.0/0",
-      description = "http_ingress",
-    type = "ingress" },
-  }
-}
