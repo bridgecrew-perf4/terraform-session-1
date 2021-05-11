4d9b2301267f15e1de25928127075ec7277b8240
nazy67 <nazykh67@gmail.com>
2021-05-11 02:35:05 +0000

    modified outputs.tf

--- session_7/frontend/outputs.tf
+++ session_7/frontend/outputs.tf
@@ -2,22 +2,22 @@ output "alb_dns" {
   value = aws_lb.web_lb.dns_name
 }
 
-# output "rds_db_name" {
-#   value = data.terraform_remote_state.rds.outputs.rds_db_name
-# }
+output "rds_db_name" {
+  value = data.terraform_remote_state.rds.outputs.rds_db_name
+}
 
-# output "rds_db_username" {
-#   value = data.terraform_remote_state.rds.outputs.rds_db_username
-# }
+output "rds_db_username" {
+  value = data.terraform_remote_state.rds.outputs.rds_db_username
+}
 
-# output "rds_db_password" {
-#   value = data.terraform_remote_state.rds.outputs.rds_password
-# }
+output "rds_db_password" {
+  value = data.terraform_remote_state.rds.outputs.rds_password
+}
 
-# output "rds_db_endpoint" {
-#   value = data.terraform_remote_state.rds.outputs.rds_endpoint
-# }
+output "rds_db_endpoint" {
+  value = data.terraform_remote_state.rds.outputs.rds_endpoint
+}
 
-output "web_sg_id" {
-  value = aws_security_group.web_sg.id
+output "webserver_sg_id" {
+  value = data.terraform_remote_state.rds.outputs.web_sg_id
 }
