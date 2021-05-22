4dcd8d8890f3656dfdf7316b85d0a180768f153a
nazy67 <nazykh67@gmail.com>
2021-05-22 03:47:06 +0000

    modified main.tf

--- session_9/dev/main.tf
+++ session_9/dev/main.tf
@@ -1,8 +1,9 @@
 module "ec2_module" {
+  # local module
   source = "../../modules/ec2"
-
-  env = "dev"
+  # variables in module
+  env           = "dev"
   instance_type = "t2.micro"
-  ami = "ami-0742b4e673072066f"
+  ami           = "ami-0742b4e673072066f"
 }
 
