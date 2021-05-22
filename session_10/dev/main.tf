8668882846a150b20b39867cc457a4b6a3f1a87c
nazy67 <nazykh67@gmail.com>
2021-05-22 16:58:45 +0000

    modified main.tf, providers.tf, added providers meta argument

--- session_10/dev/main.tf
+++ session_10/dev/main.tf
@@ -4,3 +4,14 @@ module "s3_module" {
   # variables in module
   env = "dev"
 }
+
+module "s3_module_oregon" {
+  # remote module
+  source = "github.com/nazy67/terraform-session/modules/s3"
+  # variables in module
+  env = "dev1"
+
+  provider = {
+    aws = aws.oregon
+   }
+}
--- session_10/dev/providers.tf
+++ session_10/dev/providers.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider = {
+    alias = "oregon"
+    region = "us-west-2"
+}
+
 terraform {
   required_version = "~>0.14.0"
   required_providers {
