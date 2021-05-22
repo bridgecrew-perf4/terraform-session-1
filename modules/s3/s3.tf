9ae6bd5beb113f44a0dc6039c754dbb573a009c5
nazy67 <nazykh67@gmail.com>
2021-05-22 04:49:41 +0000

    added s3.tf, locals.tf

---
+++ modules/s3/s3.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.env}-tf-nazy-bucket"
+  acl    = "private"
+
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+  ignore_public_acls = var.env == "dev" ? true : false
+  restrict_public_buckets = var.env == "dev" ? true : false
+  block_public_acls   = var.env == "dev" ? true : false
+  block_public_policy = var.env == "dev" ? true : false
+}
