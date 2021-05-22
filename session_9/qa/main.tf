b2573acfbc1e05148c2a6ca6cf692a2f25a67dcd
nazy67 <nazykh67@gmail.com>
2021-05-22 03:46:31 +0000

    modified main.tf

--- session_9/qa/main.tf
+++ session_9/qa/main.tf
@@ -1,8 +1,9 @@
 module "ec2_module" {
+  # local module
   source = "../../modules/ec2"
-
-  env = "qa"
+  # module variables
+  env           = "qa"
   instance_type = "t2.micro"
-  ami = "ami-0742b4e673072066f"
+  ami           = "ami-0742b4e673072066f"
 }
 
